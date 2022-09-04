333d9a8aaafce094c978ee172fb2175406933877
gvsarvesh <gvsarveshmern@gmail.com>
2022-09-04 20:05:37 +0000

    Conditional-Rendering

---
+++ src/App.js
@@ -0,0 +1,31 @@
+/* eslint-disable react/button-has-type */
+import {Component} from 'react'
+
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    isLoggedIn: true,
+  }
+
+  renderAuthButton = () => {
+    const {isLoggedIn} = this.state
+    if (isLoggedIn === true) {
+      return <button>Logout</button>
+    }
+    return <button>Login</button>
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Welcome greeting="Hello" name="Sarvesh" />
+        {this.renderAuthButton()}
+      </div>
+    )
+  }
+}
+
+export default App
